2232e9d93a55ddd0cca6a15781b647a5b673d3e4
menghan <7351902+menghan1994@user.noreply.gitee.com>
2025-06-06 17:03:29 +0800

    submit to github

--- main.py
+++ main.py
@@ -4,20 +4,14 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
-<<<<<<< HEAD
 def div(a, b):
     return a / b
-=======
 def mul(a, b):
     return a * b
 
->>>>>>> john_dev
 
 if __name__ == "__main__":
     print(add(1, 2))
     print(sub(1, 2))
-<<<<<<< HEAD
     print(div(1, 2))
-=======
     print(mul(1, 2))
->>>>>>> john_dev
