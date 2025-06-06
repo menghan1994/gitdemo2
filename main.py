df55b7fc2bf8b0fc5864cbf19f60616744222571
menghan <7351902+menghan1994@user.noreply.gitee.com>
2025-06-06 16:50:05 +0800

    implement multiply method

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
+def mul(a, b):
+    return a * b
+
+
 if __name__ == "__main__":
     print(add(1, 2))
     print(sub(1, 2))
+    print(mul(1, 2))
